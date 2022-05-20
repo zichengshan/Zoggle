982aa324cd4d63130756e2bd1d8d97f225098fab
Zicheng.S <zichengshan@gmail.com>
2022-05-20 15:06:06 -0700

    light && dark mode setting

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
+import {BrowserRouter as Router} from "react-router-dom"
 
 import App from './App'
 import './global.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App/>
+    <Router>
+        <App/>
+    </Router>
 )
