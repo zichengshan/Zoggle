86d0eda1427f2aa0ed296eeb0f9939a1554114c3
Zicheng.S <zichengshan@gmail.com>
2022-05-20 16:11:49 -0700

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,9 +3,10 @@ import React from "react"
 function Footer() {
     return (
         <div>
-            <h1>Footer</h1>
+            <div className='text-center p-10 mt-10 border-t dark:border-gray-700 border-gray-200'>
+                <h1>2022 Zoggle, Inc.</h1>
+            </div>
         </div>
     )
 }
-
 export default Footer
