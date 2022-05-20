127b98a941d0acf983f8dea1cf465bdd56ea0275
Zicheng.S <zichengshan@gmail.com>
2022-05-20 14:34:49 -0700

    Frontend setup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+import './global.css'
+
+function App() {
+    return (
+        <div>
+            <h1 className="text-6xlarge  underline">Hello</h1>
+        </div>
+    )
+}
+
+export default App
