252cfd0f52e03329bcc4f5f6086ac21de1b1c5ab
Zicheng.S <zichengshan@gmail.com>
2022-05-20 22:26:01 -0700

    further styling the NavLink

--- src/components/Links.jsx
+++ src/components/Links.jsx
@@ -12,7 +12,7 @@ function Links() {
     return (
         <div className='flex sm:justify-around justify-between items-center mt-4'>
             {links.map(({url, text}) => (
-                <NavLink to={url} className={(navData) => (navData.isActive ? 'text-blue-700 border-b-2 dark:text-blue-300 border-blue-700 pb-2': '')}>
+                <NavLink to={url} className={(navData) => (navData.isActive ? 'text-blue-700 border-b-2 dark:text-blue-300 border-blue-700 pb-2 m-2 mb-0': 'm-2 mb-0')}>
                     {text}
                 </NavLink>
             ))}
